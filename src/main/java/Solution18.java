e28d9d6196d00066f477fa59b8305865ccb729e2
Almond64 <almond64@knights.ucf.edu>
2021-09-18 21:31:28 -0400

    initial commit

---
+++ src/main/java/Solution18.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Press C to convert from Fahrenheit to Celsius.\nPress F to convert from Celsius to Fahrenheit.\nYour choice: ");
+        String choice = input.next();
+        //System.out.printf("%s\n", choice);
+        if (choice.equals("C")) {
+            System.out.print("Please enter the temperature in Fahrenheit: ");
+            double tempF = input.nextDouble();
+            //convert from Celsius to Fahrenheit
+            double tempC = (tempF-32)*5/9;
+            System.out.printf("The temperature in Celsius is %.2f.\n", tempC);
+        } else {
+            System.out.print("Please enter the temperature in Celsius: ");
+            double tempC = input.nextDouble();
+            //convert from Celsius to Fahrenheit
+            double tempF = (tempC*9/5)+32;
+            System.out.printf("The temperature in Celsius is %.2f.\n", tempF);
+        }
+    }
+}
